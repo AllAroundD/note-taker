7a1468eb2775318b06d908480442ee1b28c5f65a
AllAroundD <AllAroundDWeb@gmail.com>
2020-06-19 22:35:15 -0400

    fixed routing in server.js and finalized saving and deleting note

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# note-taker
-Application that can be used to write, save, and delete notes. This application will use an express backend and save and retrieve note data from a JSON file.
+<img src="https://img.shields.io/badge/AllAroundD-Note%20Taker-green">
+
+# Note-Taker
+  This is an application that can be used to write, save, and delete notes. This application will use an express backend and save and retrieve note data from a JSON file.
+
+## Description:
+  This is a web application that can write, save, and delete notes. This application uses an express backend and retrieves/saves notes from a JSON file.
+
+## Table of Contents
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contributing](#Contributing)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
+
+## Installation
+  Clone the repository and on the command line run 'npm install' to install the required modules. Next, simply run 'npm start' and the "server" will start listening on port 3000. Open a browser and go to 'http://localhost:3000/' to get to the web application.
+  Once the application loads, click the Get Started button to get to the Notes page.
+
+## Usage
+  This application is used to write, save, and delete notes.
+  Once on the Notes page, the left side of the page will show the list of notes (if any). On the right side of the page, you will have the space to enter the details of the note.
+  After entering a note, there will be a Save button in the top right corner. Once saved, the new note will be shown in the list on the left.
+  Notes can be deleted by clicking the trashcan beside the note.
+
+![note taker demo](./assets/notetaker-demo.gif)
+
+## License
+  No license
+
+## Contributing
+  This project isn't accepting any other contributions.
+
+## Questions
+![GitHub Profile Image](https://avatars3.githubusercontent.com/u/64918107?s=460&u=4277fa2bf868713adec524f08700cee517941e82&v=4)
+
+[GitHub Profile](https://github.com/AllAroundD/)
+
+-If you have any questions, please contact me at [dougmoore@use.startmail.com](mailto:dougmoore@use.startmail.com?subject=[GitHub]%20Source%20Question).
--- server.js
+++ server.js
@@ -1,34 +1,62 @@
 const express = require("express")
 const fs = require("fs")
-
+const path = require("path");
 const app = express()
 const PORT = process.env.PORT || 3000
-
-// look in 'html' FIRST and serve any static file
+const fileName = 'db/db.json'
+// look in 'public' FIRST and serve any static file
 app.use(express.static('public'))
-
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-const fileName = 'db/db.json'
-let noteList = loadNotes()
-
-function loadNotes(){
+// function to read JSON note file
+function loadNoteList(){
     const loadNotes = JSON.parse( fs.readFileSync( fileName, 'utf8') )
     return loadNotes
 }
 
-require("./routes/htmlRoutes")(app);
+// function to write back to the JSON file
+function saveNoteList(){
+    fs.writeFileSync( fileName, JSON.stringify( noteList ) )
+}
+
+// load the Note list from file
+let noteList = loadNoteList()
 
 // API routes
-app.get('/api/notes/', function(req, res) {
+// GETs
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+app.get('/api/notes', function(req, res) {
     console.log('[GET /api/notes/]')
     res.send( noteList )
 })
-
-// app.post()
+// If no matching route is found default to index
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
+// POST
+app.post('/api/notes', function(req, res) {
+    let newNote = req.body
+    // Create unique id by using date
+    newNote.id = Date.now()
+    // Add the note to the array
+    noteList.push(newNote)
+    saveNoteList()
+    console.log( `[POST /api/notes/] Saved Note id:${newNote.id}` )
+    res.send( newNote )
+})
+// DELETE
+app.delete( '/api/notes/:id', function( req, res ){
+    const noteId = req.params.id
+    noteList = noteList.filter( note=>note.id != noteId )
+    saveNoteList()
+    console.log(`[DELETE /api/notes/] deleted Note id:${noteId}` )
+    res.send( { id: noteId, message: 'Delete successful', statue: true } )
+})
 
 // listen to port
 app.listen( PORT, function(){
-    console.log( `App listening on port: ${PORT}`)
+    console.log( `App listening on PORT: ${PORT}`)
 })
